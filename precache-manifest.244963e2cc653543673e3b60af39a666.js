be5e9db6bf8cbbec42b6ebafc7a204e8135f3607
Muhammad Omar Muneer <muhammad.umar@expertflow.com>
2019-01-02 11:17:46 +0400

    Updates

---
+++ precache-manifest.244963e2cc653543673e3b60af39a666.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "442f566ddfb7708979d3",
+    "url": "/react-redux/static/css/main.ff6d8b11.chunk.css"
+  },
+  {
+    "revision": "442f566ddfb7708979d3",
+    "url": "/react-redux/static/js/main.442f566d.chunk.js"
+  },
+  {
+    "revision": "b9608fc05a561eda2fc9",
+    "url": "/react-redux/static/js/1.b9608fc0.chunk.js"
+  },
+  {
+    "revision": "ab0783d5d9c0f0db6c3f",
+    "url": "/react-redux/static/js/runtime~main.ab0783d5.js"
+  },
+  {
+    "revision": "30e97421b90033c3ee718b15ff138db7",
+    "url": "/react-redux/index.html"
+  }
+];
